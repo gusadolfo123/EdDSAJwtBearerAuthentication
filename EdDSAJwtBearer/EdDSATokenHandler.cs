50b3733fa6bf129eb6776331289fc9d014769c7b
Gustavo Moreno <gusadolfo123@hotmail.com>
2021-07-16 17:30:20 -0500

    finalizando helper EdDSA

--- EdDSAJwtBearer/EdDSATokenHandler.cs
+++ EdDSAJwtBearer/EdDSATokenHandler.cs
@@ -1,6 +1,10 @@
 ﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -96,10 +100,86 @@ namespace EdDSAJwtBearer
 
         private static EdDSAKeys GetDerEncodedAsymmetricCipherKeyPair(AsymmetricCipherKeyPair keys)  
         {
-            var privateKey = GetDerDecodedAsymemetricPrivateKeyParameter(keys.Private);
-            var publicKey = GetDerDecodedAsymemetricPublicKeyParameter(keys.Public);
+            var edDSAKeys = new EdDSAKeys();
 
-            return new EdDSAKeys(publicKey, privateKey);
+            var privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(keys.Private);
+            byte[] buffer = privateKeyInfo.ToAsn1Object().GetDerEncoded();
+            edDSAKeys.Private = Convert.ToBase64String(buffer);
+
+            var publicKeyInfo = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(keys.Public);
+            buffer = publicKeyInfo.ToAsn1Object().GetDerEncoded();
+            edDSAKeys.Public = Convert.ToBase64String(buffer);
+
+            return edDSAKeys;
+        }
+
+        private static AsymmetricCipherKeyPair CreateKeys()
+        {
+            var keyPairGenerator = new Ed25519KeyPairGenerator();
+            keyPairGenerator.Init(new Ed25519KeyGenerationParameters(new SecureRandom()));
+
+            return keyPairGenerator.GenerateKeyPair();
+        }
+
+        public static EdDSAKeys CreateDerEncodeKey()
+        {
+            return GetDerEncodedAsymmetricCipherKeyPair(CreateKeys());
+        }
+
+        public static bool VerifySignature(string token, string edDSAPublicKey)
+        {
+            bool result = false;
+
+            try
+            {
+                string[] JWTParts = token.Split('.');
+                if (JWTParts.Length == 3)
+                {
+                    string data = $"{JWTParts[0]}.{JWTParts[1]}";
+                    byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+                    byte[] signature = Base64UrlDecode(JWTParts[2]);
+
+                    var validator = new Ed25519Signer();
+                    validator.Init(false, GetDerDecodedAsymemetricPublicKeyParameter(edDSAPublicKey));
+                    validator.BlockUpdate(dataBytes, 0, dataBytes.Length);
+                     
+                    result = validator.VerifySignature(signature);
+                }
+            }
+            catch (Exception)
+            { 
+                // logica cuando el token no puede ser verificado
+                throw;
+            }
+
+            return result;
+        }
+
+        public static bool TryGetPayloadFromToken(
+            string token, 
+            string edDSAPublicKey, 
+            out Dictionary<string, object> payload)
+        {
+            bool result = false;
+            payload = null;
+
+            try
+            {
+                if (VerifySignature(token, edDSAPublicKey))
+                {
+                    string payloadData = token.Split(".")[1];
+                    string JSONPayload = Encoding.UTF8.GetString(Base64UrlDecode(payloadData));
+                    payload = JsonSerializer.Deserialize<Dictionary<string, object>>(JSONPayload);
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                // no se pudo obtener el contenido
+                throw;
+            }
+
+            return result;
         }
 
         public static string Base64UrlEncode(byte[] arg)
