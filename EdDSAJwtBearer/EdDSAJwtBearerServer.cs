48cf6475e673371f0dd618015bd35f989d111e4a
Gustavo Moreno <gusadolfo123@hotmail.com>
2021-07-17 14:58:28 -0500

    asd

---
+++ EdDSAJwtBearer/EdDSAJwtBearerServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace EdDSAJwtBearer
+{
+    public class EdDSAJwtBearerServer
+    { 
+        public EdDSAJwtBearerServerOptions EdDSAJwtBearerServerOptions { get; set; }
+
+        public EdDSAJwtBearerServer()
+        {
+        }
+
+        public EdDSAJwtBearerServer(EdDSAJwtBearerServerOptions options)
+        {
+            EdDSAJwtBearerServerOptions = options;
+        }
+
+
+    }
+}
